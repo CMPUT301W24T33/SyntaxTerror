cb599c6fe7b965d34226ee5730a548499f96490c
Ian <ihmorris@ualberta.ca>
2024-02-21 10:39:55 -0700

    updated AnonAuth

--- app/src/main/java/com/example/eventalate/MainActivity.java
+++ app/src/main/java/com/example/eventalate/MainActivity.java
@@ -13,6 +13,15 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.auth.FirebaseAuthCredentialsProvider;
+
 
 import java.util.HashMap;
 
@@ -20,10 +29,14 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseFirestore db;
     private CollectionReference events;
     private Event event;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
 
         event = new Event("FolkFest", "Folky");
 
@@ -46,5 +59,7 @@ public class MainActivity extends AppCompatActivity {
                 });
 
 
+
     }
+
 }
