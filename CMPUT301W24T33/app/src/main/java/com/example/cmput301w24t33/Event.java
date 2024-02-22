eb2d610e3bd199cbcd7e3c9940909697aa459699
CJBlackduck <cblackdu@ualberta.ca>
2024-02-21 18:25:34 -0700

    added first chunk of UI

---
+++ CMPUT301W24T33/app/src/main/java/com/example/cmput301w24t33/Event.java
@@ -0,0 +1,17 @@
+package com.example.cmput301w24t33;
+
+public class Event {
+    String eventText;
+
+    public Event(String eventText) {
+        this.eventText = eventText;
+    }
+
+    public String getEventText() {
+        return eventText;
+    }
+
+    public void setEventText(String eventText) {
+        this.eventText = eventText;
+    }
+}
