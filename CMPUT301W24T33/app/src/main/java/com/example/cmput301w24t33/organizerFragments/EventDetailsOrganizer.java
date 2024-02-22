990c715b43548cfed8ffcf47e6ddf36468950a73
CJBlackduck <cblackdu@ualberta.ca>
2024-02-22 15:30:09 -0700

    a

--- CMPUT301W24T33/app/src/main/java/com/example/cmput301w24t33/organizerFragments/EventDetailsOrganizer.java
+++ CMPUT301W24T33/app/src/main/java/com/example/cmput301w24t33/organizerFragments/EventDetailsOrganizer.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.cmput301w24t33.R;
 
@@ -61,6 +63,19 @@ public class EventDetailsOrganizer extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.organizer_event_fragment, container, false);
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.organizer_event_fragment,null);
+        setOnClickListeners(view);
+        return view;
+    }
+    private void setOnClickListeners(View view){
+        ImageButton shareButton = view.findViewById(R.id.share_button);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // set share functionality here
+                Log.d("share","working");
+            }
+        });
+
     }
 }
