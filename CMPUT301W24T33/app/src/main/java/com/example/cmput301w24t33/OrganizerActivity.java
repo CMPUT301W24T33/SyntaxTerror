ee2eda19a929eb114c09ac56d7696ce651d223e3
David Schroeder <djschroe@ualberta.ca>
2024-02-21 20:18:48 -0700

    UI evening development synchronization checkpoint

---
+++ CMPUT301W24T33/app/src/main/java/com/example/cmput301w24t33/OrganizerActivity.java
@@ -0,0 +1,72 @@
+package com.example.cmput301w24t33;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class OrganizerActivity extends AppCompatActivity implements AdapterEventClickListener{
+    private ArrayList<Event> organizedEvents;
+    private RecyclerView eventRecyclerView;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.organizer_activity);
+        eventRecyclerView = findViewById(R.id.organized_events);
+        setEvents();
+        setAdapter();
+        ImageView profileButton = findViewById(R.id.profile_image);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new ProfileFragment());
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.attendee_layout,fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    private void setEvents(){
+        organizedEvents = new ArrayList<>();
+        organizedEvents.add(new Event("Test1"));
+        organizedEvents.add(new Event("Test2"));
+        organizedEvents.add(new Event("Test3"));
+        organizedEvents.add(new Event("Test4"));
+        organizedEvents.add(new Event("Test5"));
+        organizedEvents.add(new Event("Test6"));
+        organizedEvents.add(new Event("Test7"));
+        organizedEvents.add(new Event("Test8"));
+        organizedEvents.add(new Event("Test9"));
+        organizedEvents.add(new Event("Test10"));
+        organizedEvents.add(new Event("Test11"));
+        organizedEvents.add(new Event("Test12"));
+    }
+
+    private void setAdapter(){
+        eventRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        eventRecyclerView.setHasFixedSize(true);
+        EventAdapter eventAdapter = new EventAdapter(organizedEvents,this);
+        eventRecyclerView.setAdapter(eventAdapter);
+        eventAdapter.notifyDataSetChanged();
+    }
+    
+    @Override
+    public void onEventClickListener(Event event, int position) {
+        replaceFragment(new OrganizerEventFragment());
+    }
+}
