da06665125937d7e1ce3e3c549163109850485d3
David Schroeder <djschroe@ualberta.ca>
2024-03-03 18:53:05 -0700

    Refactored attendees event view java and xml. Also refactored Organizer create event java.

--- CMPUT301W24T33/app/src/main/java/com/example/cmput301w24t33/organizerFragments/CreateEvent.java
+++ CMPUT301W24T33/app/src/main/java/com/example/cmput301w24t33/organizerFragments/CreateEvent.java
@@ -3,119 +3,133 @@ package com.example.cmput301w24t33.organizerFragments;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.example.cmput301w24t33.R;
+import com.example.cmput301w24t33.databinding.OrganizerCreateEventFragmentBinding;
 import com.example.cmput301w24t33.events.EventChooseQR;
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Calendar;
+import java.util.Locale;
+import java.util.Objects;
 
 public class CreateEvent extends Fragment {
 
-    public static CreateEvent editEvent(String param1) {
-        CreateEvent fragment = new CreateEvent();
-        Bundle args = new Bundle();
-        // Add data to bundle here as follows
-        // args.putString(ARG_PARAM1, param1);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private OrganizerCreateEventFragmentBinding binding;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = OrganizerCreateEventFragmentBinding.inflate(inflater, container, false);
+        setupUI();
+        return binding.getRoot();
+    }
+
+    private void setupUI() {
+        setupActionButtons();
+        setupDateTimePickers();
+    }
+
+    private void setupActionButtons() {
+        binding.cancelButton.setOnClickListener(v -> onCancel());
+        binding.confirmButton.setOnClickListener(v -> onConfirm());
+        binding.uploadPosterButton.setOnClickListener(v -> onUploadPoster());
+        binding.generateQrCodeButton.setOnClickListener(v -> onSelectQRCode());
+    }
+
+    private void onCancel() {
+        // Handle the cancel action
+        binding.cancelButton.setOnClickListener(v -> getParentFragmentManager().popBackStack());
+        Snackbar.make(binding.getRoot(), "Event creation cancelled", Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void onConfirm() {
+        // Collect data from input fields
+        String eventName = Objects.requireNonNull(binding.eventNameEditText.getText()).toString().trim();
+        String eventDescription = Objects.requireNonNull(binding.eventDescriptionEditText.getText()).toString().trim();
+        String startDate = Objects.requireNonNull(binding.startDateEditText.getText()).toString().trim();
+        String startTime = Objects.requireNonNull(binding.startTimeEditText.getText()).toString().trim();
+        String endDate = Objects.requireNonNull(binding.endDateEditText.getText()).toString().trim();
+        String endTime = Objects.requireNonNull(binding.endTimeEditText.getText()).toString().trim();
+        String maxAttendees = Objects.requireNonNull(binding.maxAttendeesEditText.getText()).toString().trim();
+        boolean geoLocationTracking = binding.geoTrackingSwitch.isChecked();
+
+        // Validate input
+        if (eventName.isEmpty() || eventDescription.isEmpty() || startDate.isEmpty() || startTime.isEmpty() || endDate.isEmpty() || endTime.isEmpty()) {
+            Snackbar.make(binding.getRoot(), "Please fill in all fields", Snackbar.LENGTH_LONG).show();
+            return;
         }
+
+        // Database Code Here
+        Snackbar.make(binding.getRoot(), "Event Created", Snackbar.LENGTH_SHORT).show();
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.organizer_create_event_fragment, container, false);
-        setupClickListeners(view);
-        return view;
+    private void onUploadPoster() {
+        // Handle the poster upload
     }
 
-    private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.organizer_layout,fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
+    private void onSelectQRCode() {
+        // Handle QR Code selection
+        binding.generateQrCodeButton.setOnClickListener(v -> replaceFragment(new EventChooseQR()));
     }
 
-    private void setupClickListeners(View view){
-        setupDateTimeClickListeners(view);
-
-        // Cancel Event Creation
-        MaterialButton cancel = view.findViewById(R.id.cancel_button);
-        cancel.setOnClickListener(v -> getParentFragmentManager().popBackStack());
-
-        // Confirm Event Creation
-        MaterialButton confirm = view.findViewById(R.id.confirm_button);
-        confirm.setOnClickListener(v -> getParentFragmentManager().popBackStack());
-
-        // Upload Event Poster
-        MaterialButton uploadPoster = view.findViewById(R.id.upload_poster_button);
-//        uploadPoster.setOnClickListener();
-
-        // Generate Event QR code
-        MaterialButton generateQR = view.findViewById(R.id.generate_qr_code_button);
-        generateQR.setOnClickListener(v -> replaceFragment(new EventChooseQR()));
+    private void setupDateTimePickers() {
+        binding.startDateEditText.setOnClickListener(v -> showDatePickerDialog(true));
+        binding.startTimeEditText.setOnClickListener(v -> showTimePickerDialog(true));
+        binding.endDateEditText.setOnClickListener(v -> showDatePickerDialog(false));
+        binding.endTimeEditText.setOnClickListener(v -> showTimePickerDialog(false));
     }
 
-    private void setupDateTimeClickListeners(View view) {
-        TextInputEditText startDate = view.findViewById(R.id.start_date_edit_text);
-        TextInputEditText startTime = view.findViewById(R.id.start_time_edit_text);
-        TextInputEditText endDate = view.findViewById(R.id.end_date_edit_text);
-        TextInputEditText endTime = view.findViewById(R.id.end_time_edit_text);
-
-        // Set click listeners
-        startDate.setOnClickListener(v -> showDatePickerDialog(startDate));
-        startTime.setOnClickListener(v -> showTimePickerDialog(startTime));
-        endDate.setOnClickListener(v -> showDatePickerDialog(endDate));
-        endTime.setOnClickListener(v -> showTimePickerDialog(endTime));
-    }
-
-    private void showDatePickerDialog(TextInputEditText editText) {
+    private void showDatePickerDialog(boolean isStart) {
         final Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH);
         int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-        DatePickerDialog datePickerDialog = new DatePickerDialog(requireContext(),
-                (view, year1, monthOfYear, dayOfMonth) -> {
-                    // Format the date as you like
-                    String selectedDate = year1 + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
-                    editText.setText(selectedDate);
-                }, year, month, day);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(requireContext(), (view, year1, monthOfYear, dayOfMonth) -> {
+            String selectedDate = String.format(Locale.getDefault(), "%d-%02d-%02d", year1, monthOfYear + 1, dayOfMonth);
+            if (isStart) {
+                binding.startDateEditText.setText(selectedDate);
+            } else {
+                binding.endDateEditText.setText(selectedDate);
+            }
+        }, year, month, day);
+
         datePickerDialog.show();
     }
 
-    private void showTimePickerDialog(TextInputEditText editText) {
+    private void showTimePickerDialog(boolean isStart) {
         final Calendar calendar = Calendar.getInstance();
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
         int minute = calendar.get(Calendar.MINUTE);
 
-        TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(),
-                (view, hourOfDay, minute1) -> {
-                    // Format the time as you like
-                    String selectedTime = hourOfDay + ":" + minute1;
-                    editText.setText(selectedTime);
-                }, hour, minute, DateFormat.is24HourFormat(getContext()));
+        TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), (view, hourOfDay, minuteOfHour) -> {
+            String selectedTime = String.format(Locale.getDefault(), "%02d:%02d", hourOfDay, minuteOfHour);
+            if (isStart) {
+                binding.startTimeEditText.setText(selectedTime);
+            } else {
+                binding.endTimeEditText.setText(selectedTime);
+            }
+        }, hour, minute, DateFormat.is24HourFormat(getContext()));
+
         timePickerDialog.show();
     }
 
-}
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 
+    private void replaceFragment(Fragment fragment) {
+        getParentFragmentManager().beginTransaction()
+                .replace(R.id.attendee_layout, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+}
