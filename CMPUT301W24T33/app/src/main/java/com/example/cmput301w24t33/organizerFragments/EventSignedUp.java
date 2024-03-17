04aa48afd5f6b7f56ccc569df0e833a7bac3fa93
David Schroeder <djschroe@ualberta.ca>
2024-03-17 12:05:09 -0600

    Organizer event details view signups display  implemented.

---
+++ CMPUT301W24T33/app/src/main/java/com/example/cmput301w24t33/organizerFragments/EventSignedUp.java
@@ -0,0 +1,66 @@
+// Purpose:
+//
+//
+// Issues:
+//
+
+package com.example.cmput301w24t33.organizerFragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.cmput301w24t33.R;
+import com.example.cmput301w24t33.users.User;
+import com.example.cmput301w24t33.databinding.OrganizerEventSignedUpFragmentBinding;
+
+import java.util.ArrayList;
+
+public class EventSignedUp extends Fragment {
+    private OrganizerEventSignedUpFragmentBinding binding;
+    public static EventSignedUp newInstance(ArrayList<User> signedUpList) {
+        Bundle args = new Bundle();
+        args.putSerializable("signedUpList", signedUpList);
+        EventSignedUp frag = new EventSignedUp();
+        frag.setArguments(args);
+        return frag;
+    }
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = OrganizerEventSignedUpFragmentBinding.inflate(inflater, container, false);
+        setupActionBar();
+        setupEventSignUpsRecyclerView();
+        return binding.getRoot();
+    }
+    private void setupEventSignUpsRecyclerView() {
+        assert getArguments() != null;
+        ArrayList<User> signedUpList = (ArrayList<User>) getArguments().getSerializable("signedUpList");
+        binding.eventSignupsList.setLayoutManager(new LinearLayoutManager(getContext()));
+        AttendeeAdapter adapter = new AttendeeAdapter(signedUpList);
+        binding.eventSignupsList.setAdapter(adapter);
+        binding.signedupCount.setText(String.valueOf(signedUpList.size()));
+    }
+    private void setupActionBar() {
+        TextView actionBarText = binding.actionbar.findViewById(R.id.back_actionbar_textview);
+        actionBarText.setText("Signed Up");
+
+        int color = ContextCompat.getColor(getContext(), R.color.organizer_actionbar);
+
+        ImageButton backButton = binding.actionbar.findViewById(R.id.back_arrow_img);
+        backButton.setOnClickListener(v -> getParentFragmentManager().popBackStack());
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
